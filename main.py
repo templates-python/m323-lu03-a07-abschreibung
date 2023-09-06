d0b65cf8702128fec286a345bec59d6a3fd3db81
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-06 11:30:48 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-def calculate():
+def calculate_depreciation(initial_value, depreciation_rate, years):
     """
-    calculates the total
-    :return: None
+    Berechnet den Restwert einer Anlage nach einer bestimmten Anzahl von Jahren unter Berücksichtigung der jährlichen Abschreibung.
+
+    Args:
+    initial_value (float): Der Anfangswert der Anlage.
+    depreciation_rate (float): Der jährliche Abschreibungssatz als Dezimalzahl (z.B. 0.10 für 10%).
+    years (int): Die Anzahl der Jahre, für die die Abschreibung berechnet werden soll.
+
+    Returns:
+    float: Der Restwert der Anlage nach der gegebenen Anzahl von Jahren.
+
+    Raises:
+    ValueError: Wird geworfen, wenn der Abschreibungssatz nicht zwischen 0 und 1 liegt.
+    ValueError: Wird geworfen, wenn die Anzahl der Jahre negativ ist.
     """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    pass
 
 
 if __name__ == '__main__':
-    calculate()
+    print(calculate_depreciation(20000,0.1,5))
--- main_test.py
+++ main_test.py
@@ -1,6 +1,39 @@
 import main
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_calculate_depreciation_normal_case():
+    result = main.calculate_depreciation(1000, 0.1, 5)
+    assert round(result, 2) == 590.49, f"Expected 590.49, got {result}"
+
+def test_calculate_depreciation_zero_years():
+    result = main.calculate_depreciation(1000, 0.1, 0)
+    assert result == 1000, f"Expected 1000, got {result}"
+
+def test_calculate_depreciation_zero_depreciation():
+    result = main.calculate_depreciation(1000, 0.0, 5)
+    assert result == 1000, f"Expected 1000, got {result}"
+
+def test_calculate_depreciation_one_depreciation():
+    result = main.calculate_depreciation(1000, 1.0, 5)
+    assert result == 0, f"Expected 0, got {result}"
+
+def test_calculate_depreciation_negative_initial_value():
+    result = main.calculate_depreciation(-1000, 0.1, 5)
+    assert round(result, 2) == -590.49, f"Expected -590.49, got {result}"
+
+def test_calculate_depreciation_invalid_depreciation_rate_low():
+    try:
+        main.calculate_depreciation(1000, -0.1, 5)
+    except ValueError as e:
+        assert str(e) == "Der Abschreibungssatz muss zwischen 0 und 1 liegen."
+
+def test_calculate_depreciation_invalid_depreciation_rate_high():
+    try:
+        main.calculate_depreciation(1000, 1.1, 5)
+    except ValueError as e:
+        assert str(e) == "Der Abschreibungssatz muss zwischen 0 und 1 liegen."
+
+def test_calculate_depreciation_negative_years():
+    try:
+        main.calculate_depreciation(1000, 0.1, -1)
+    except ValueError as e:
+        assert str(e) == "Die Anzahl der Jahre kann nicht negativ sein."
